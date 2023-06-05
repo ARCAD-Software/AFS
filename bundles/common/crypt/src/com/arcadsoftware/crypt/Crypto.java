f0d8d27a3e48bc21d74b05d1bd7718c843204ace
mlafon <mlafon@arcadsoftware.com>
2023-06-05 15:53:03 +0200

    Missing copyright comments...

--- bundles/common/crypt/src/com/arcadsoftware/crypt/Crypto.java
+++ bundles/common/crypt/src/com/arcadsoftware/crypt/Crypto.java
@@ -1,10 +1,16 @@
-/**
- * Copyright (c) Arcad-Software (2008). All Rights Reserved.
- * 
- * Creation date: 27 juin 2008
- * @author ARCAD Software
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2023 ARCAD Software.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ARCAD Software - initial API and implementation
+ *******************************************************************************/
 package com.arcadsoftware.crypt;
 
 import java.io.ByteArrayOutputStream;
