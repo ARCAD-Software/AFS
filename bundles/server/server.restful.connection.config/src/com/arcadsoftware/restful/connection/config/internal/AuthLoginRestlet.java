e92a6b35bbf66e5c84c97025e4b80d834fc84443
mlafon <mlafon@arcadsoftware.com>
2023-08-31 10:10:34 +0200

    Add about.html and comment header in c.a.s.restful.connection.config

--- bundles/server/server.restful.connection.config/src/com/arcadsoftware/restful/connection/config/internal/AuthLoginRestlet.java
+++ bundles/server/server.restful.connection.config/src/com/arcadsoftware/restful/connection/config/internal/AuthLoginRestlet.java
@@ -1,10 +1,16 @@
-/**
- * Copyright (c) Arcad-Software (2009). All Rights Reserved.
- * 
- * Creation date: 17 sept. 2009
- * @author ARCAD Software
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2023 ARCAD Software.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ARCAD Software - initial API and implementation
+ *******************************************************************************/
 package com.arcadsoftware.restful.connection.config.internal;
 
 import org.restlet.Context;
