e8b482a94d30ae2814fdca5e6f0a4d376351372b
mlafon <mlafon@arcadsoftware.com>
2024-03-28 14:40:27 +0100

    Fix copyrights

--- bundles/server/server.restful.log/src/com/arcadsoftware/restlog/LogZipResource.java
+++ bundles/server/server.restful.log/src/com/arcadsoftware/restlog/LogZipResource.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2024 ARCAD Software.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     ARCAD Software - initial API and implementation
+ *******************************************************************************/
 package com.arcadsoftware.restlog;
 
 import java.io.File;
