7a1abf5aae4f40025205ce6ac71a4b23d45a05a8
mlafon <mlafon@arcadsoftware.com>
2024-06-05 15:11:12 +0200

    Delete binaries file associated to an data deleted.
    fix #61

--- bundles/server/metadata.binary/src/com/arcadsoftware/metadata/binary/Activator.java
+++ bundles/server/metadata.binary/src/com/arcadsoftware/metadata/binary/Activator.java
@@ -14,10 +14,16 @@
 package com.arcadsoftware.metadata.binary;
 
 import org.osgi.framework.BundleContext;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
 import org.restlet.Context;
 import org.restlet.routing.Router;
 
+import com.arcadsoftware.beanmap.BeanMap;
+import com.arcadsoftware.metadata.MetaDataEntity;
+import com.arcadsoftware.metadata.MetaDataEventHandler;
 import com.arcadsoftware.osgi.AbstractActivator;
+import com.arcadsoftware.osgi.IBinariesTranferService;
 import com.arcadsoftware.rest.RouteList;
 import com.arcadsoftware.rest.SimpleBranch;
 
@@ -33,15 +39,36 @@ public class Activator extends AbstractActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		context = bundleContext;
 		super.start(bundleContext);
+		// Register Web-service branch.
 		registerService(SimpleBranch.clazz, new SimpleBranch() {
-			
 			protected RouteList createAttachedResources(Context context, Router router) {
 				RouteList routes = new RouteList(router.attach("/bin", BinaryRedirectionResource.class)); //$NON-NLS-1$
 				routes.add(router.attach("/bin/{binkey}", BinaryRedirectionResource.class)); //$NON-NLS-1$
 				return routes;
 			}
-			
 		}, SimpleBranch.properties("/data/type")); //$NON-NLS-1$
+		// Register deletion listener.
+		registerService(EventHandler.class, new MetaDataEventHandler(this) {
+			@Override
+			public void handleEvent(Event event) {
+				MetaDataEntity entity = getEntity(event);
+				BeanMap item = getBeanMap(event);
+				if ((entity != null) && (item != null)) {
+					String category = entity.getMetadata().getString("binary"); //$NON-NLS-1$
+					if ((category != null) && !category.isEmpty()) {
+						Object o = event.getProperty(MetaDataEventHandler.EVENT_PROP_HARDDELETE);
+						if ((o instanceof Boolean) && ((Boolean) o)) {
+							IBinariesTranferService bin = Activator.this.getService(IBinariesTranferService.class);
+							if (bin != null) {
+								for (String c: category.split(" ")) { //$NON-NLS-1$
+									bin.removeFile(c, item.getId());
+								}
+							}
+						}
+					}
+				}
+			}
+		}, MetaDataEventHandler.getProperties(MetaDataEventHandler.TOPIC_BEANMAP_DELETED));
 	}
 
 }
