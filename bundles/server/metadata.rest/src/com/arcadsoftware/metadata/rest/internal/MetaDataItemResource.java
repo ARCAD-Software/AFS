3823957f86a5ae07f9291b050f22074a1527e92c
mlafon <mlafon@arcadsoftware.com>
2024-07-31 09:44:13 +0200

    MetaDataEntity API ehancement
    Allow to get the reference lines in a Form through the formToBean
    method.
    
    Add, as a side note, a method convertValue in the MetaDataAttribute
    class allowing to convert any String value in a more accurate class,
    according to the attribute type.

--- bundles/server/metadata.rest/src/com/arcadsoftware/metadata/rest/internal/MetaDataItemResource.java
+++ bundles/server/metadata.rest/src/com/arcadsoftware/metadata/rest/internal/MetaDataItemResource.java
@@ -329,6 +329,10 @@ public class MetaDataItemResource extends DataItemResource {
 
 	@Override
 	protected Representation delete(Variant variant) throws ResourceException {
+		if (!isExisting()) {
+			setStatus(Status.SUCCESS_NO_CONTENT);
+			return null;
+		}
 		Language language = getClientPreferedLanguage();
 		Form form = getRequestForm();
 		if (linkeds != null) {
@@ -638,6 +642,10 @@ public class MetaDataItemResource extends DataItemResource {
 
 	@Override
 	protected Representation put(Representation representation, Variant variant) throws ResourceException {
+		if (!isExisting()) {
+			setStatus(Status.CLIENT_ERROR_NOT_FOUND);
+			return null;
+		}
 		Language language = getClientPreferedLanguage();		
 		Form form = getRequestForm();
 		if (link != null) {
