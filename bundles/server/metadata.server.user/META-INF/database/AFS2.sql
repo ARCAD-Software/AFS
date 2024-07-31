8cba400547b92b95d981e3d3cefed4ec093b37d5
mlafon <mlafon@arcadsoftware.com>
2024-07-31 10:01:49 +0200

    Missing FK in the database create script.

--- bundles/server/metadata.server.user/META-INF/database/AFS2.sql
+++ bundles/server/metadata.server.user/META-INF/database/AFS2.sql
@@ -1,6 +1,8 @@
 -- User Management Tables
 --
 
+alter table USERS add constraint USR_UST_ID_FK foreign key (USR_TITLE) references USER_TITLES (UST_ID) on delete set null;
+
 create index PRF_CODE_INDEX on PROFILES (PRF_CODE, PRF_DELETED);
 
 create index PRF_CODE_INDEX on PROFILE_RIGHTS (PFR_PRF_ID, PFR_DELETED);
