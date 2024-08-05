2e43c1ca6f84bc34dbf74c304ace198cf6d270a5
Christophe Yang <cyang@arcadsoftware.com>
2024-08-05 11:29:30 +0200

    AFS2 sql typo (index doublon) correction

--- bundles/server/metadata.server.user/META-INF/database/AFS2.sql
+++ bundles/server/metadata.server.user/META-INF/database/AFS2.sql
@@ -5,7 +5,7 @@ alter table USERS add constraint USR_UST_ID_FK foreign key (USR_TITLE) reference
 
 create index PRF_CODE_INDEX on PROFILES (PRF_CODE, PRF_DELETED);
 
-create index PRF_CODE_INDEX on PROFILE_RIGHTS (PFR_PRF_ID, PFR_DELETED);
+create index PFR_CODE_INDEX on PROFILE_RIGHTS (PFR_PRF_ID, PFR_DELETED);
 
 --
 -- End of User Management
