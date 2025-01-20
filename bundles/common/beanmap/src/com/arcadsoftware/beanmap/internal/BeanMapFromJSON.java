e3fd014f879f2d9983415f17ee87d740e234b884
mlafon <mlafon@arcadsoftware.com>
2025-01-20 14:42:07 +0100

    BeanMap API deserialize BeanMapList contained in a BeanMap from a JSON array.

--- bundles/common/beanmap/src/com/arcadsoftware/beanmap/internal/BeanMapFromJSON.java
+++ bundles/common/beanmap/src/com/arcadsoftware/beanmap/internal/BeanMapFromJSON.java
@@ -3,10 +3,12 @@ package com.arcadsoftware.beanmap.internal;
 import java.text.ParseException;
 import java.util.Iterator;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import com.arcadsoftware.beanmap.BeanMap;
+import com.arcadsoftware.beanmap.BeanMapList;
 import com.arcadsoftware.osgi.ISODateFormater;
 
 public final class BeanMapFromJSON {
@@ -31,6 +33,12 @@ public final class BeanMapFromJSON {
 				Object x = o.get(k);
 				if (x instanceof JSONObject) {
 					bean.put(k, parse(new BeanMap(k), (JSONObject) x));
+				} else if (x instanceof JSONArray) {
+					BeanMapList list = new BeanMapList();
+					for (int i = 0; i < ((JSONArray) x).length(); i++) {
+						list.add(parse(new BeanMap(), ((JSONArray) x).getJSONObject(i)));
+					}
+					bean.put(k, list);
 				} else {
 					bean.put(k, x);
 				}
@@ -38,7 +46,7 @@ public final class BeanMapFromJSON {
 		}
 		return bean;
 	}
-	
+
 	private BeanMapFromJSON() {}
 
 }
