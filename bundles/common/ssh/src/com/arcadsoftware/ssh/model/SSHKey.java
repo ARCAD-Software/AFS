d1016bfb65da582635b0552f4e229806cff1ce1c
mlafon <mlafon@arcadsoftware.com>
2025-05-28 14:45:08 +0200

    Typo and comments

--- bundles/common/ssh/src/com/arcadsoftware/ssh/model/SSHKey.java
+++ bundles/common/ssh/src/com/arcadsoftware/ssh/model/SSHKey.java
@@ -16,91 +16,219 @@ package com.arcadsoftware.ssh.model;
 import com.arcadsoftware.beanmap.BeanMap;
 import com.arcadsoftware.beanmap.IIdentifiedBean;
 
+/**
+ * Represents an SSH key with its associated properties.
+ * <p>
+ * This class implements the {@link IIdentifiedBean} interface and uses a {@link BeanMap}
+ * to store and manage the key's attributes such as name, type, length, fingerprint, and passphrase.
+ * </p>
+ * 
+ * @author ARCAD Software
+ */
 public class SSHKey implements IIdentifiedBean {
 
-	public static final String ENTITY = "sshKey"; //$NON-NLS-1$
-	public static final String NAME = "name"; //$NON-NLS-1$
-	public static final String FINGERPRINT = "fingerprint"; //$NON-NLS-1$
-	public static final String TYPE = "keytype"; //$NON-NLS-1$
-	public static final String LENGTH = "keylength"; //$NON-NLS-1$
-	public static final String COMMENT = "keycomment"; //$NON-NLS-1$
-	public static final String PASSPHRASE = "passphrase"; //$NON-NLS-1$
+    /**
+     * The entity name used in the underlying {@link BeanMap}.
+     */
+    public static final String ENTITY = "sshKey"; //$NON-NLS-1$
 
-	private final BeanMap beanmap;
+    /**
+     * The property key for the SSH key's name.
+     */
+    public static final String NAME = "name"; //$NON-NLS-1$
 
-	public SSHKey() {
-		this(new BeanMap(ENTITY));
-	}
+    /**
+     * The property key for the SSH key's fingerprint.
+     */
+    public static final String FINGERPRINT = "fingerprint"; //$NON-NLS-1$
 
-	public SSHKey(String type) {
-		this(new BeanMap(type));
-	}
+    /**
+     * The property key for the SSH key's type.
+     */
+    public static final String TYPE = "keytype"; //$NON-NLS-1$
 
-	public SSHKey(final BeanMap beanmap) {
-		this.beanmap = beanmap;
-		if (beanmap == null) {
-			throw new NullPointerException("A BeanMap is required when creating a SSHKey !");
-		}
-	}
+    /**
+     * The property key for the SSH key's length.
+     */
+    public static final String LENGTH = "keylength"; //$NON-NLS-1$
 
-	public BeanMap getBeanMap() {
-		return beanmap;
-	}
+    /**
+     * The property key for the SSH key's comment.
+     */
+    public static final String COMMENT = "keycomment"; //$NON-NLS-1$
 
-	public String getComment() {
-		return beanmap.getString(COMMENT);
-	}
+    /**
+     * The property key for the SSH key's passphrase.
+     */
+    public static final String PASSPHRASE = "passphrase"; //$NON-NLS-1$
 
-	public String getFingerprint() {
-		return beanmap.getString(FINGERPRINT);
-	}
+    /**
+     * The underlying map storing SSH key properties.
+     */
+    private final BeanMap beanmap;
 
-	@Override
-	public int getId() {
-		return beanmap.getId();
-	}
+    /**
+     * Constructs a new {@code SSHKey} with a default empty {@link BeanMap}.
+     */
+    public SSHKey() {
+        this(new BeanMap(ENTITY));
+    }
 
-	public int getLength() {
-		return beanmap.getInt(LENGTH);
-	}
+    /**
+     * Constructs a new {@code SSHKey} initialized with a {@link BeanMap} identified by the given type string.
+     *
+     * @param type the type name to initialize the {@link BeanMap}
+     */
+    public SSHKey(String type) {
+        this(new BeanMap(type));
+    }
 
-	public String getName() {
-		return beanmap.getString(NAME);
-	}
+    /**
+     * Constructs a new {@code SSHKey} using the specified {@link BeanMap}.
+     *
+     * @param beanmap the bean map holding SSH key properties, must not be null
+     * @throws NullPointerException if the {@code beanmap} is null
+     */
+    public SSHKey(final BeanMap beanmap) {
+        this.beanmap = beanmap;
+        if (beanmap == null) {
+            throw new NullPointerException("A BeanMap is required when creating a SSHKey !");
+        }
+    }
 
-	public String getPassphrase() {
-		return beanmap.getString(PASSPHRASE);
-	}
+    /**
+     * Returns the underlying {@link BeanMap} containing SSH key properties.
+     *
+     * @return the bean map of this SSH key
+     */
+    public BeanMap getBeanMap() {
+        return beanmap;
+    }
 
-	public SSHKeyType getType() {
-		return SSHKeyType.fromName(beanmap.getString(TYPE));
-	}
+    /**
+     * Returns the comment associated with the SSH key.
+     *
+     * @return the key comment, or null if not set
+     */
+    public String getComment() {
+        return beanmap.getString(COMMENT);
+    }
 
-	public boolean isEncrypted() {
-		return (getPassphrase() != null) && !getPassphrase().isEmpty();
-	}
+    /**
+     * Returns the fingerprint of the SSH key.
+     *
+     * @return the fingerprint string, or null if not set
+     */
+    public String getFingerprint() {
+        return beanmap.getString(FINGERPRINT);
+    }
 
-	public void setComment(final String comment) {
-		beanmap.put(COMMENT, comment);
-	}
+    /**
+     * Returns the unique identifier of this SSH key.
+     *
+     * @return the ID of the SSH key
+     */
+    @Override
+    public int getId() {
+        return beanmap.getId();
+    }
 
-	public void setFingerprint(final String fingerprint) {
-		beanmap.put(FINGERPRINT, fingerprint);
-	}
+    /**
+     * Returns the length (in bits) of the SSH key.
+     *
+     * @return the key length
+     */
+    public int getLength() {
+        return beanmap.getInt(LENGTH);
+    }
 
-	public void setLength(final int length) {
-		beanmap.put(LENGTH, length);
-	}
+    /**
+     * Returns the name of the SSH key.
+     *
+     * @return the key name, or null if not set
+     */
+    public String getName() {
+        return beanmap.getString(NAME);
+    }
 
-	public void setName(final String name) {
-		beanmap.put(NAME, name);
-	}
+    /**
+     * Returns the passphrase protecting the SSH key, if any.
+     *
+     * @return the passphrase, or null if not set
+     */
+    public String getPassphrase() {
+        return beanmap.getString(PASSPHRASE);
+    }
 
-	public void setPassphrase(final String passphrase) {
-		beanmap.put(PASSPHRASE, passphrase);
-	}
+    /**
+     * Returns the type of the SSH key.
+     *
+     * @return the {@link SSHKeyType} of the key, or null if type is not set or invalid
+     */
+    public SSHKeyType getType() {
+        return SSHKeyType.fromName(beanmap.getString(TYPE));
+    }
 
-	public void setType(final SSHKeyType type) {
-		beanmap.put(TYPE, type.getName());
-	}
+    /**
+     * Indicates whether the SSH key is encrypted with a passphrase.
+     *
+     * @return {@code true} if the key has a non-empty passphrase; {@code false} otherwise
+     */
+    public boolean isEncrypted() {
+        return (getPassphrase() != null) && !getPassphrase().isEmpty();
+    }
+
+    /**
+     * Sets the comment for this SSH key.
+     *
+     * @param comment the comment string to set
+     */
+    public void setComment(final String comment) {
+        beanmap.put(COMMENT, comment);
+    }
+
+    /**
+     * Sets the fingerprint for this SSH key.
+     *
+     * @param fingerprint the fingerprint string to set
+     */
+    public void setFingerprint(final String fingerprint) {
+        beanmap.put(FINGERPRINT, fingerprint);
+    }
+
+    /**
+     * Sets the length (in bits) for this SSH key.
+     *
+     * @param length the key length to set
+     */
+    public void setLength(final int length) {
+        beanmap.put(LENGTH, length);
+    }
+
+    /**
+     * Sets the name for this SSH key.
+     *
+     * @param name the name string to set
+     */
+    public void setName(final String name) {
+        beanmap.put(NAME, name);
+    }
+
+    /**
+     * Sets the passphrase for this SSH key.
+     *
+     * @param passphrase the passphrase string to set
+     */
+    public void setPassphrase(final String passphrase) {
+        beanmap.put(PASSPHRASE, passphrase);
+    }
+
+    /**
+     * Sets the type of this SSH key.
+     *
+     * @param type the {@link SSHKeyType} to set
+     */
+    public void setType(final SSHKeyType type) {
+        beanmap.put(TYPE, type.getName());
+    }
 }
--- bundles/common/ssh/src/com/arcadsoftware/ssh/model/SSHKeyType.java
+++ bundles/common/ssh/src/com/arcadsoftware/ssh/model/SSHKeyType.java
@@ -13,71 +13,149 @@
  *******************************************************************************/
 package com.arcadsoftware.ssh.model;
 
+/**
+ * Enumeration representing supported SSH key types.
+ * <p>
+ * Each enum constant defines the properties of an SSH key type,
+ * including its name, algorithm, key length, key file names, and cryptographic provider.
+ * </p>
+ * 
+ * @author ARCAD Software
+ */
 public enum SSHKeyType {
-	
-	UNKNOWN("", "", 0, "", "", ""),
-	RSA("RSA", "RSA", 4096, "id_rsa", "id_rsa.pub", "BC"),
-	EDDSA("EdDSA", "Ed25519", 255, "id_ed25519", "id_ed25519.pub", "BC");
 
-	public static SSHKeyType fromAlgorithm(final String algo) {
-		if (algo != null && ! algo.isEmpty()) {
-			for (SSHKeyType skt : SSHKeyType.values()) {
-				if (skt.getAlgorithm().equalsIgnoreCase(algo)) {
-					return skt;
-				}
-			}
-		}
-		return UNKNOWN;
-	}
-	
-	public static SSHKeyType fromName(final String name) {
-		if (name != null && !name.isEmpty()) {
-			for (SSHKeyType skt : SSHKeyType.values()) {
-				if (skt.getName().equalsIgnoreCase(name)) {
-					return skt;
-				}
-			}
-		}
-		return UNKNOWN;
-	}
-	
-	private final String name;
-	private final String algorithm;
-	private final int length;
-	private final String privateKeyName;
-	private final String publicKeyName;
-	private final String provider;
+    /**
+     * Unknown or unsupported SSH key type.
+     */
+    UNKNOWN("", "", 0, "", "", ""),
 
-	SSHKeyType(final String name, final String algorithm, final int length, final String privateKeyName, final String publicKeyName, final String provider) {
-		this.name = name;
-		this.algorithm = algorithm;
-		this.length = length;
-		this.privateKeyName = privateKeyName;
-		this.publicKeyName = publicKeyName;
-		this.provider = provider;
-	}
+    /**
+     * RSA SSH key type.
+     */
+    RSA("RSA", "RSA", 4096, "id_rsa", "id_rsa.pub", "BC"),
 
-	public String getName() {
-		return name;
-	}
-	
-	public String getAlgorithm() {
-		return algorithm;
-	}
+    /**
+     * EdDSA SSH key type (Ed25519 variant).
+     */
+    EDDSA("EdDSA", "Ed25519", 255, "id_ed25519", "id_ed25519.pub", "BC");
 
-	public int getLength() {
-		return length;
-	}
+    /**
+     * Returns the {@link SSHKeyType} corresponding to the given algorithm name.
+     * <p>
+     * The comparison is case-insensitive.
+     * </p>
+     *
+     * @param algo the algorithm name to look up
+     * @return the matching {@code SSHKeyType}, or {@link #UNKNOWN} if no match is found
+     */
+    public static SSHKeyType fromAlgorithm(final String algo) {
+        if ((algo != null) && !algo.isBlank()) {
+            for (SSHKeyType skt : SSHKeyType.values()) {
+                if (skt.getAlgorithm().equalsIgnoreCase(algo)) {
+                    return skt;
+                }
+            }
+        }
+        return UNKNOWN;
+    }
 
-	public String getPrivateKeyName() {
-		return privateKeyName;
-	}
+    /**
+     * Returns the {@link SSHKeyType} corresponding to the given type name.
+     * <p>
+     * The comparison is case-insensitive.
+     * </p>
+     *
+     * @param name the type name to look up
+     * @return the matching {@code SSHKeyType}, or {@link #UNKNOWN} if no match is found
+     */
+    public static SSHKeyType fromName(final String name) {
+        if ((name != null) && !name.isBlank()) {
+            for (SSHKeyType skt : SSHKeyType.values()) {
+                if (skt.getName().equalsIgnoreCase(name)) {
+                    return skt;
+                }
+            }
+        }
+        return UNKNOWN;
+    }
 
-	public String getPublicKeyName() {
-		return publicKeyName;
-	}
-	
-	public String getProvider() {
-		return provider;
-	}
+    private final String name;
+    private final String algorithm;
+    private final int length;
+    private final String privateKeyName;
+    private final String publicKeyName;
+    private final String provider;
+
+    /**
+     * Constructs an SSHKeyType enum instance.
+     *
+     * @param name the display name of the key type
+     * @param algorithm the algorithm name
+     * @param length the default key length (in bits)
+     * @param privateKeyName the default private key file name
+     * @param publicKeyName the default public key file name
+     * @param provider the cryptographic provider name
+     */
+    SSHKeyType(final String name, final String algorithm, final int length, final String privateKeyName, final String publicKeyName, final String provider) {
+        this.name = name;
+        this.algorithm = algorithm;
+        this.length = length;
+        this.privateKeyName = privateKeyName;
+        this.publicKeyName = publicKeyName;
+        this.provider = provider;
+    }
+
+    /**
+     * Returns the display name of this SSH key type.
+     *
+     * @return the key type name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the algorithm name associated with this SSH key type.
+     *
+     * @return the algorithm name
+     */
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    /**
+     * Returns the default key length (in bits) for this SSH key type.
+     *
+     * @return the key length
+     */
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * Returns the default private key file name for this SSH key type.
+     *
+     * @return the private key file name
+     */
+    public String getPrivateKeyName() {
+        return privateKeyName;
+    }
+
+    /**
+     * Returns the default public key file name for this SSH key type.
+     *
+     * @return the public key file name
+     */
+    public String getPublicKeyName() {
+        return publicKeyName;
+    }
+
+    /**
+     * Returns the cryptographic provider name associated with this SSH key type.
+     *
+     * @return the provider name
+     */
+    public String getProvider() {
+        return provider;
+    }
 }
