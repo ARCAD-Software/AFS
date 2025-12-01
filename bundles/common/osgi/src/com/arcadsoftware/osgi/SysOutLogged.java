661ccfd4b65660dcc309ef305ba224646c0fb1f6
mlafon <mlafon@arcadsoftware.com>
2025-12-01 17:11:58 +0100

    fix sysoutlogged

--- bundles/common/osgi/src/com/arcadsoftware/osgi/SysOutLogged.java
+++ bundles/common/osgi/src/com/arcadsoftware/osgi/SysOutLogged.java
@@ -23,10 +23,12 @@ package com.arcadsoftware.osgi;
  */
 public class SysOutLogged implements ILoggedPlugin {
 
+	@Override
 	public final void log(String message) {
 		log(message, null);
 	}
 
+	@Override
 	public void log(String message, Throwable e) {
 		if (message != null) {
 			System.out.println(message);
@@ -36,10 +38,12 @@ public class SysOutLogged implements ILoggedPlugin {
 		}
 	}
 
+	@Override
 	public final void log(Throwable e) {
 		log(e.getLocalizedMessage(), e);
 	}
 
+	@Override
 	public void error(String message, Throwable e) {
 		if (message != null) {
 			System.err.println(message);
@@ -51,22 +55,22 @@ public class SysOutLogged implements ILoggedPlugin {
 		}
 	}
 
+	@Override
 	public final void error(String message) {
-		if (message != null) {
-			System.err.println(message);
-		}
+		error(message, (Throwable) null);
 	}
 
+	@Override
 	public final void error(Throwable e) {
 		error(e.getLocalizedMessage(), e);
 	}
 
+	@Override
 	public final void warn(String message) {
-		if (message != null) {
-			System.err.println("[WARN] " + message); //$NON-NLS-1$
-		}
+		warn(message, (Throwable) null);
 	}
 
+	@Override
 	public void warn(String message, Throwable e) {
 		if (message != null) {
 			System.err.println("[WARN] " + message); //$NON-NLS-1$
@@ -76,14 +80,17 @@ public class SysOutLogged implements ILoggedPlugin {
 		}
 	}
 
+	@Override
 	public final void warn(Throwable e) {
 		warn(e.getLocalizedMessage(), e);
 	}
 
+	@Override
 	public final void debug(String message) {
-		debug(message, null);
+		debug(message, (Throwable) null);
 	}
 
+	@Override
 	public void debug(String message, Throwable e) {
 		if (message != null) {
 			System.out.println("[DEBUG] " + message); //$NON-NLS-1$
@@ -93,30 +100,29 @@ public class SysOutLogged implements ILoggedPlugin {
 		}
 	}
 
+	@Override
 	public final void debug(Throwable e) {
 		debug(e.getLocalizedMessage(), e);
 	}
 
 	@Override
-	public void info(String message) {
+	public final void info(String message) {
 		log(message);
 	}
 
 	@Override
-	public void info(String message, Throwable e) {
+	public final void info(String message, Throwable e) {
 		log(message, e);
 	}
 
 	@Override
-	public void info(Throwable e) {
+	public final void info(Throwable e) {
 		info(e.getLocalizedMessage(), e);
 	}
 
 	@Override
-	public void trace(String message) {
-		if (message != null) {
-			System.out.println("[TRACE] " + message); //$NON-NLS-1$
-		}
+	public final void trace(String message) {
+		trace(message, (Throwable) null);
 	}
 
 	@Override
@@ -130,7 +136,7 @@ public class SysOutLogged implements ILoggedPlugin {
 	}
 
 	@Override
-	public void trace(Throwable e) {
+	public final void trace(Throwable e) {
 		trace(e.getLocalizedMessage(), e);
 	}
 
@@ -145,14 +151,12 @@ public class SysOutLogged implements ILoggedPlugin {
 	}
 
 	@Override
-	public void audit(String message) {
-		if (message != null) {
-			System.err.println("[AUDIT] " + message); //$NON-NLS-1$
-		}
+	public final void audit(String message) {
+		audit(message, (Throwable) null);
 	}
 
 	@Override
-	public void audit(Throwable e) {
+	public final void audit(Throwable e) {
 		audit(e.getLocalizedMessage(), e);
 	}
 
